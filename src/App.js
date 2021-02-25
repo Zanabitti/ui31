ab90044773604a9d1f57e970f4dbe7a59c30a2bd
Zanabitti <krepper_9@hotmail.com>
2021-02-25 10:16:37 +0200

    Dragging windows

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import "./App.css";
 import Window from './components/window/Window'
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        VIEWPORTHEIGHT : window.innerHeight,
+        VIEWPORTWIDTH : window.innerWidth
+    }
+  }
+
   render(){
+    const appStyle = {
+      height: this.state.VIEWPORTHEIGHT + 'px',
+      width: '100%'
+    };
     return(
-      <div className="App">
+      <div className="App" style={appStyle}>
         <Window />
       </div>
     );
--- src/components/window/Window.js
+++ src/components/window/Window.js
@@ -5,11 +5,49 @@ import './Window.css';
 class Window extends React.Component {
     constructor(props){
         super(props);
+        this.refWindow = React.createRef();
+    }
+    
+
+    drag(e){
+        if(e.target.className === 'window-header') {
+            e.preventDefault();
+            e.stopPropagation();
+            let prevXY;
+            const onMove = (e) => {
+                
+
+                if(!prevXY) {
+                    prevXY = [e.clientX, e.clientY];
+                }
+
+                const [x, y] = prevXY;
+                const [diffX, diffY] = [e.clientX - x, e.clientY - y];
+                const currX = parseInt(this.refWindow.current.style.left) || 0;
+                const currY = parseInt(this.refWindow.current.style.top) || 0;
+    
+                this.refWindow.current.style.left = (currX + diffX) + 'px';
+                this.refWindow.current.style.top = (currY + diffY) + 'px';
+
+                prevXY = [e.clientX, e.clientY];
+            }
+            const onDone = (e) => {
+                this.refWindow.current.removeEventListener("mousemove", onMove);
+                this.refWindow.current.removeEventListener("mouseup", onDone);           
+            }
+            this.refWindow.current.addEventListener("mousemove", onMove);
+            this.refWindow.current.addEventListener("mouseup", onDone);
+        }
+    }
+
+    dragStart(e){
+        this.msStartX = e.clientX;
+        this.msStartY = e.clientY;
     }
 
     render() {
         return (
-            <div className="window-border">
+            <div className="window-border"  onDragStart={this.dragStart.bind(this)} onMouseDown={this.drag.bind(this)} onDragEnd={null} ref={this.refWindow}>
                 <div className="window-inner">
                     <Header />
                 </div>
