94760e756b5eac96ab51085bf2213baa3343c670
Zanabitti <krepper_9@hotmail.com>
2021-03-02 04:06:02 +0200

    production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,17 @@ const path = require("path");
 const webpack = require("webpack");
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        excludeChunks: [ 'server' ]
+      }),    
+   ],
 });
